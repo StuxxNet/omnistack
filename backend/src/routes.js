dc09da75cad873f1e4b773b423ac2d85a0cc9285
Ramon Borges <RamonBoorges@gmail.com>
2019-08-06 22:36:31 -0300

    Finished the method to like a developer in the Likes controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const routes = express.Router();
 const DevController = require("./controllers/DevController");
+const LikesController = require("./controllers/LikesController");
 
 routes.post("/devs", DevController.store);
+routes.post("/devs/:devId/likes", LikesController.store);
 
 module.exports = routes;
