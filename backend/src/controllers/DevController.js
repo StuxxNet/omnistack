b2c7f62a7091aa07a299d5ac5587c4003b5d81e7
Ramon Borges <RamonBoorges@gmail.com>
2019-08-06 22:58:58 -0300

    Added a method to list all the developers that weren't added to the likes neither to the dislikes list

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,21 @@ const axios = require("axios");
 const Dev = require("../models/DevModule")
 
 module.exports = {
+    async index(req, res){
+        const { user } = req.headers;
+        
+        const loggedDev = await Dev.findById(user);
+        console.log(loggedDev._id);
+        const users = await Dev.find({
+            $and: [
+                { _id: { $ne: loggedDev._id } },
+                { _id: { $nin: loggedDev.likes } },
+                { _id: { $nin: loggedDev.dislikes } },
+            ]
+        })
+
+        return res.json(users);
+    },
     async store(req,res){
         const { username } = req.body;
         const userExists = await Dev.findOne({ user: username });
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ const DevController = require("./controllers/DevController");
 const LikesController = require("./controllers/LikesController");
 const DislikesController = require("./controllers/DislikesController");
 
+routes.get("/devs", DevController.index);
+
 routes.post("/devs", DevController.store);
 routes.post("/devs/:devId/likes", LikesController.store);
 routes.post("/devs/:devId/dislikes", DislikesController.store);
